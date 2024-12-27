d7d4cabb80c491d5f98fe3d62c4a2e19ff084b75
Tejender Dubey <tejender.dubey@indiamart.com>
2024-12-27 22:23:27 +0530

    header page

---
+++ src/pages/Home.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <div className="header-section">
+        <div className="df jc_sb pd0_10 ai_c h100p">
+          <div className="nav-title">SkillQuest</div>
+          <div className="nav-heading">JavaScript Interview Preparation</div>
+          <div className="nav-message">Welcome !!</div>
+        </div>
+      </div>
+
+      <div className="mid-section pd0_10"></div>
+
+      <div className="footer-section">
+        <div className="df jc_c pd0_10 ai_c h100p">
+          <button className="button-bottom-01 fff cp">Let's Get Started</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
