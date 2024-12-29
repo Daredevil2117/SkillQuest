957618be189dd0ff97e24cdc5be8e980a6628d41
Tejender Dubey <tejender.dubey@indiamart.com>
2024-12-29 20:06:22 +0530

    tutorial page handling

--- src/components/OutlinePanel.js
+++ src/components/OutlinePanel.js
@@ -1,15 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
 function OutlinePanel({tutorials, onSelectArticle}) {
+  const [selected, setSelected] = useState(null);
   return (
-    <div className="panel">
-      <div>Js Interview Questions !!</div>
+    <div className="panel" style={{position:"relative"}}>
+      <div style={{position:"absolute", right:"0", fontSize:"10px"}}>
+        Toggle ME
+      </div>
+      <div className="pd10" style={{fontWeight:"600", fontSize:"1.2rem"}}>Js Interview Questions !!</div>
       <div>
-        {tutorials.map((item) => (
+        {tutorials.map((item,index) => (
           <div
             key={item.id}
-            onClick={() => onSelectArticle(item)}
-            className="outline-item"
+            onClick={() => {onSelectArticle(item);setSelected(index)}}
+            className="outline-item cp"
+            style={{
+              padding:"3px 14px",
+              fontWeight: "400",
+              backgroundColor: selected === index ? "#CADCFC" : "#f3eeee",
+              fontSize:"1.1rem",
+            }}
+            onMouseEnter={(e) => {
+              if (selected !== index) e.target.style.backgroundColor = "#cccccc";
+            }}
+            onMouseLeave={(e) => {
+              if (selected !== index) e.target.style.backgroundColor = "#f3eeee";
+            }}
           >
             {item.title}
           </div>
